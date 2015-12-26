b34ca81045ccb9340149600e30247d2a0fe6653f
Tom Servo <rachelpage921@gmail.com>
2015-12-26 12:32:31 -0600

    random text in the middle

--- src/Landscape.elm
+++ src/Landscape.elm
@@ -2,6 +2,7 @@ module Landscape where
 
 import Graphics.Element as E exposing (Element)
 import Graphics.Collage as C exposing (Form)
+import Text
 
 main : Element
 main = C.collage 1000 748 forms
@@ -9,8 +10,11 @@ main = C.collage 1000 748 forms
 forms : List Form
 forms = 
   [
-    C.toForm background
+    C.toForm background,
+    C.text someText
   ]
+
+someText = Text.fromString "wat"
 
 
 background = E.image 1000 748 "../images/solarsystem.png"
