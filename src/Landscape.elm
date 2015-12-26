ca519cff9b293daff49c29518070e215e1e4a3a0
Tom Servo <rachelpage921@gmail.com>
2015-12-26 12:38:53 -0600

    Refactor: use HTML

--- src/Landscape.elm
+++ src/Landscape.elm
@@ -3,9 +3,10 @@ module Landscape where
 import Graphics.Element as E exposing (Element)
 import Graphics.Collage as C exposing (Form)
 import Text
+import Html exposing (Html)
 
-main : Element
-main = C.collage 1000 748 forms
+main : Html
+main = C.collage 1000 748 forms |> Html.fromElement
 
 forms : List Form
 forms = 
