8060f235f0315fb1d0e6894ce8ba9c9fc257001d
Tom Servo <rachelpage921@gmail.com>
2015-12-26 12:48:42 -0600

    pass model to view

--- src/Landscape.elm
+++ src/Landscape.elm
@@ -4,18 +4,21 @@ import Graphics.Element as E exposing (Element)
 import Graphics.Collage as C exposing (Form)
 import Text
 import Html exposing (Html)
+import Signal exposing (Signal)
 
-main : Html
-main = C.collage 1000 748 forms |> Html.fromElement
+type alias Model = (Int,Int)
 
-forms : List Form
-forms = 
+main : Signal Html
+main = Signal.constant (view (0,0))
+
+view : Model -> Html
+view pointer = C.collage 1000 748 (forms pointer) |> Html.fromElement
+
+forms : Model -> List Form
+forms pointer = 
   [
     C.toForm background,
-    C.text someText
+    C.text (Text.fromString (toString pointer))
   ]
-
-someText = Text.fromString "wat"
-
 
 background = E.image 1000 748 "../images/solarsystem.png"
