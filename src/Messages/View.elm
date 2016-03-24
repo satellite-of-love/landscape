76213639805882cc0c98929bae656b7c375700b4
Jessica Kerr <jessitron@stripe.com>
2016-03-23 21:41:10 -0500

    move aside style to css file

--- src/Messages/View.elm
+++ src/Messages/View.elm
@@ -7,14 +7,5 @@ import Html.Attributes as Attr
 messagePane : Int -> List String -> Html
 messagePane height whatToSay =
   Html.aside
-    [ Attr.style
-        [ ( "width", "200px" )
-        , ( "height", "100%" )
-        , ( "border", "medium dashed green" )
-        , ( "position", "relative" )
-        , ( "left", "1000" )
-        , ( "vertical-align", "top" )
-        , ( "overflow", "scroll" )
-        ]
-    ]
+    []
     [ Html.ul [] (List.map (\a -> Html.li [] [ Html.text a ]) whatToSay) ]
