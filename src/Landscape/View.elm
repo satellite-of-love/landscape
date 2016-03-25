9ed337efcd39be697548f7dfee4c99fe76efcf4e
Jessica Kerr <jessitron@stripe.com>
2016-03-25 18:20:20 -0500

    remove Graphics
    and do relative size
    unfortunately this skew pixels

--- src/Landscape/View.elm
+++ src/Landscape/View.elm
@@ -1,33 +1,29 @@
 module Landscape.View (landscapePane) where
 
-import Graphics.Element as E exposing (Element)
-import Graphics.Collage as C exposing (Form)
 import Text
 import Html exposing (Html)
 import Html.Attributes as Attr
 import Landscape.Model exposing (Model, MousePosition)
 
 
+landscapePane : Int -> Model -> Html
 landscapePane height model =
-  Html.div
-    [ Attr.style
-        [ ( "position", "absolute" )
-        ]
-    ]
-    [ landscape height model.pointer ]
-
-
-landscape : Int -> MousePosition -> Html
-landscape height model =
-  C.collage 1000 height (forms height model) |> Html.fromElement
-
-
-forms : Int -> MousePosition -> List Form
-forms height model =
-  [ C.toForm (background height)
-  , C.text (Text.fromString (toString model))
+  Html.div []
+  [
+    Html.canvas [][],
+    mousePointerText model
   ]
 
 
-background height =
-  E.image 1000 height "images/solarsystem.png"
+mousePointerText : Model -> Html
+mousePointerText model =
+  Html.div
+   [ Attr.style
+      [ ( "position", "absolute" ),
+        ( "background", "images/solarsystem.png"),
+        ( "top", "500px"),
+        ( "left", "396px")
+      ]
+    ]
+    [ Html.output [] [Html.text (toString model.pointer)]]
+
