01d1823291532bb365e26c47a897fb3410b4f465
Jessica Kerr <jessitron@stripe.com>
2016-03-26 16:22:16 -0500

    moved the canvas. my god this is hard

--- src/LandscapeCss.elm
+++ src/LandscapeCss.elm
@@ -1,7 +1,23 @@
 module LandscapeCss (css) where
 
 import Css exposing (overflow, scroll, border3, verticalAlign, height, width, groove, top, left, vw, vh, stylesheet, px, rgb, backgroundColor, margin, padding, position, absolute)
-import Css.Elements exposing (aside)
+import Css.Elements exposing (canvas, aside, ul)
+
+
+fullHeight =
+  (vh 100)
+
+
+landscapeWidth =
+  (vw 70)
+
+
+messagesWidth =
+  (vw 30)
+
+
+veryTop =
+  (px 0)
 
 
 css =
@@ -11,15 +27,28 @@ css =
         , margin (px 0)
         , padding (px 0)
         , position absolute
-        , top (vh 0)
-        , left (vw 70)
-        , height (vh 100)
-        , width (vw 30)
+        , top veryTop
+        , left landscapeWidth
+        , height fullHeight
+        , width messagesWidth
         , border3 mediumBorder groove green
         , verticalAlign top
         , overflow scroll
         ]
+    , canvas
+        [ position absolute
+        , top veryTop
+        , left (px 0)
+        , backgroundImageUrl "images/solarsystem.png"
+        , Css.property "background-size" "100% 100%"
+        , width landscapeWidth
+        , height fullHeight
+        ]
     ]
+
+
+backgroundImageUrl url =
+  Css.property "background-image" ("url(" ++ url ++ ")")
 
 
 white =
