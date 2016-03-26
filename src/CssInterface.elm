ac115600505fb6b0f0c757e0471497fdce394f07
Jessica Kerr <jessitron@stripe.com>
2016-03-26 11:07:46 -0500

    this is not ready

---
+++ src/CssInterface.elm
@@ -0,0 +1,7 @@
+module CssInterface (..) where
+
+import Html.CssHelpers exposing (withNamespace)
+
+
+cssNamespace =
+  withNamespace "landscape"
---
+++ src/LandscapeCss.elm
@@ -0,0 +1,29 @@
+module LandscapeCss (css) where
+
+import Css exposing (..)
+import Css.Elements exposing (..)
+import Css.Namespace exposing (namespace)
+import CssInterface exposing (cssNamespace)
+
+
+css =
+  (stylesheet << namespace cssNamespace.name)
+    [ header
+        [ backgroundColor (rgb 90 90 90)
+        , boxSizing borderBox
+        , padding (px -80)
+        ]
+    ]
+
+aside {
+  background-color: white;
+  margin: 0;
+  padding: 0;
+  left: 70%;
+  width: 30%;
+  height: 100%;
+  border: medium groove green;
+  position: absolute;
+  vertical-align: top;
+  overflow: scroll;
+}
