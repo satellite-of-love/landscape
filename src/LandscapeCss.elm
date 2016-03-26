eedc6de322e7aa258473e18a3564e5aebd23028f
Jessica Kerr <jessitron@stripe.com>
2016-03-26 11:12:52 -0500

    not there yet

--- src/LandscapeCss.elm
+++ src/LandscapeCss.elm
@@ -7,23 +7,23 @@ import CssInterface exposing (cssNamespace)
 
 
 css =
-  (stylesheet << namespace cssNamespace.name)
-    [ header
+  stylesheet
+    [ aside
         [ backgroundColor (rgb 90 90 90)
         , boxSizing borderBox
         , padding (px -80)
         ]
     ]
 
-aside {
-  background-color: white;
-  margin: 0;
-  padding: 0;
-  left: 70%;
-  width: 30%;
-  height: 100%;
-  border: medium groove green;
-  position: absolute;
-  vertical-align: top;
-  overflow: scroll;
-}
+--aside {
+--  background-color: white;
+--  margin: 0;
+--  padding: 0;
+--  left: 70%;
+--  width: 30%;
+--  height: 100%;
+--  border: medium groove green;
+--  position: absolute;
+--  vertical-align: top;
+--  overflow: scroll;
+--}
