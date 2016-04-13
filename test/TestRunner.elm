35b560a49f1b423ca8319b0152ca4cbebed936a1
Jessica Kerr <jessitron@outpace.com>
2016-04-13 17:09:05 +0000

    Now you can run tests with elm-make

--- test/TestRunner.elm
+++ test/TestRunner.elm
@@ -3,6 +3,7 @@ module Main (..) where
 import Signal exposing (Signal)
 import ElmTest exposing (consoleRunner)
 import Console exposing (IO, run)
+import Graphics.Element exposing (Element)
 import Task
 import Tests
 
@@ -15,3 +16,8 @@ console =
 port runner : Signal (Task.Task x ())
 port runner =
   run console
+
+
+main : Element
+main =
+  ElmTest.elementRunner Tests.all
