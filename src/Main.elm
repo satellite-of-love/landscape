2ccd85d1e02d52aa0e683b22ca010c0110aa5cde
Jessica Kerr <jessitron@outpace.com>
2016-04-13 17:30:15 +0000

    Extract top-level Update module

--- src/Main.elm
+++ src/Main.elm
@@ -11,13 +11,10 @@ import Set exposing (Set)
 import Model exposing (Model, MousePosition, OutsideWorld, ApplicationState)
 import Clock exposing (Clock)
 import Action exposing (Action, News(..), OutgoingNews)
+import Update exposing (updateModel)
 import Landscape.View
-import Landscape.Update
-import Messages.Update
 import Messages.View
-import TextInput.Update
 import TextInput.View
-import NewsInjector.Update
 import NewsInjector.View
 import Serialization
 import Json.Encode
@@ -41,6 +38,11 @@ main : Signal Html
 main =
   app.modelSignal
     |> Signal.map (view (Signal.forwardTo newsFromTheView.address DoThis))
+
+
+dropTheSecondInput : (a -> b -> ( b, List c )) -> a -> ( b, List c ) -> ( b, List c )
+dropTheSecondInput f a ( b, c ) =
+  f a b
 
 
 
@@ -88,109 +90,8 @@ mouseClicks =
 
 
 -----
--- UPDATE
+-- VIEW
 -----
-
-
-updateModel : News Action OutgoingNews -> Model -> ( Model, List OutgoingNews )
-updateModel news model =
-  let
-    ( model, allTheNews ) =
-      NewsInjector.Update.pretendThingsHappened model news
-  in
-    List.foldl (appendSecondOutput respondToOneNews) ( model, [] ) allTheNews
-
-
-respondToOneNews : News Action OutgoingNews -> Model -> ( Model, List OutgoingNews )
-respondToOneNews news model =
-  let
-    world =
-      retainOutsideWorld news model.world
-
-    spiedOnNews =
-      Messages.Update.spyOnNews model.clock world news model.state
-
-    actions =
-      respondToNews news world
-
-    spiedState =
-      List.foldl (Messages.Update.spyOnActions model.clock) spiedOnNews actions
-
-    ( actedState, outgoingNews ) =
-      List.foldl (respondToAction model.clock) ( spiedState, [] ) actions
-
-    finalState =
-      List.foldl (Messages.Update.spyOnOutgoingNews model.clock) actedState outgoingNews
-  in
-    ( { world = world, state = finalState, clock = model.clock + 1 }, outgoingNews )
-
-
-updateOneIgnoreAnother : (b -> b) -> (( b, c ) -> ( b, c ))
-updateOneIgnoreAnother f ( one, another ) =
-  ( f one, another )
-
-
-appendSecondOutput : (a -> b -> ( b, List c )) -> a -> ( b, List c ) -> ( b, List c )
-appendSecondOutput f a =
-  updateOneSumAnother (f a)
-
-
-dropTheSecondInput : (a -> b -> ( b, List c )) -> a -> ( b, List c ) -> ( b, List c )
-dropTheSecondInput f a ( b, c ) =
-  f a b
-
-
-updateOneSumAnother : (b -> ( b, List c )) -> (( b, List c ) -> ( b, List c ))
-updateOneSumAnother f ( one, others ) =
-  let
-    ( newOne, moreOthers ) =
-      f one
-  in
-    ( newOne, others ++ moreOthers )
-
-
-explicitActions news =
-  case news of
-    DoThis action ->
-      [ action ]
-
-    _ ->
-      []
-
-
-retainOutsideWorld news model =
-  case news of
-    MouseMove spot keys ->
-      let
-        before =
-          model.keysDown
-      in
-        { model
-          | pointer = spot
-          , keysDown = keys
-          , previousKeysDown = before
-        }
-
-    _ ->
-      model
-
-
-respondToNews : News Action OutgoingNews -> OutsideWorld -> List Action
-respondToNews news world =
-  (Landscape.Update.seeTheWorld news world)
-    ++ (TextInput.Update.seeTheWorld news world)
-    ++ (Messages.Update.seeTheWorld news world)
-    ++ (NewsInjector.Update.seeTheWorld news world)
-    ++ (explicitActions news)
-
-
-respondToAction : Clock -> Action -> ( ApplicationState, List OutgoingNews ) -> ( ApplicationState, List OutgoingNews )
-respondToAction clock action ( state, outgoingNews ) =
-  ( state, outgoingNews )
-    |> updateOneSumAnother (TextInput.Update.respondToActions clock action)
-    |> updateOneIgnoreAnother (Landscape.Update.respondToActions action)
-    |> updateOneIgnoreAnother (NewsInjector.Update.respondToActions action)
-    |> updateOneIgnoreAnother (Messages.Update.respondToActions action)
 
 
 view : Signal.Address Action -> Model -> Html
