2300d3258c5d8ebcb38f806609865abe70fb35cc
Jessica Kerr <jessitron@outpace.com>
2016-04-16 14:13:05 +0000

    shows mouse position for checking

--- test/HowDoTransformsWork.elm
+++ test/HowDoTransformsWork.elm
@@ -4,11 +4,28 @@ import Html exposing (Html)
 import Html.Attributes as Attr
 import LandscapeCss exposing (absolutePositionAtCenter)
 import Landscape.Calculations as Calculations
-import Html.CssHelpers as Help
+import Mouse
+import Window
+import Signal exposing (Signal)
 
 
-main : Html
+main : Signal Html
 main =
+  relativeMousePosition
+    |> Signal.map view
+
+
+view p =
+  Html.div
+    [ Attr.style [ ( "position", "absolute" ), ( "top", "0" ), ( "left", "0" ) ] ]
+    [ showMousePos p, oneTransformCheck ]
+
+
+showMousePos p =
+  Html.text (toString p)
+
+
+oneTransformCheck =
   let
     cssTransformation =
       { scale = 1, translateX = 0, translateY = 0 }
@@ -22,3 +39,17 @@ main =
           )
       ]
       [ Html.text (toString cssTransformation) ]
+
+
+type alias PositionOnScreen =
+  ( Int, Int )
+
+
+relativeMousePosition : Signal PositionOnScreen
+relativeMousePosition =
+  Signal.map3 relativize Mouse.position Window.width Window.height
+
+
+relativize : ( Int, Int ) -> Int -> Int -> PositionOnScreen
+relativize ( x, y ) w h =
+  ( x * 100 // w, y * 100 // h )
